a00f5d2f19f1816eb8723651fd62f040ea1e69c9
electronicstinkerer <electronicstinkerer@gmail.com>
2020-12-26 23:09:54 -0500

    Updated client js for faster message response Also: * Delete a message no longer requires a page reload * Post a message no longer requires a page reload * Fix: you must now be logged in to post messages * Separated CSS to separate file (and lots of fixes) * Multi-line messages are now possible * Changed message input box to a "textarea" for multi-line edits * Added message-deletion confirmation box * Changed on-load scrolling to "auto" instead of "smooth" * Improved tokenizing input message (switched explode() to preg_split())

--- messenger.php
+++ messenger.php
@@ -6,6 +6,9 @@
     const TAGS_KEY    = "tags";
     const MESSAGE_FILE = "messenger.json";
 
+    // Debug
+    $console_output = "";
+
     // Flags
     $message_filter = "";
 
@@ -57,23 +60,18 @@
 
         // Generate the HTML for the entire message (TODO: Maybe remove a lot of this whitespace (indentation) to reduce bandwidth?)
         return "
-            <div class='single-message'>
+            <div class='single-message' id='{$message_id}'>
                 <hr>
                 <p class='name'>{$message[DEVICE_KEY]}</p>
                 <p class='time'>".date('D, M d - h:i A', $message[TIME_KEY])."</p>
                 <div class='tags-container'>{$tags_html}</div>
-                <form action='' method='post' class='inline-form'>
-                    <input type='text' value='{$message_id}' name='message-id' style='display: none;'>
-                    <input type='submit' name='delete-message' value='Delete'>
-                </form>
-                <div class='message' onclick='copyMessage()' id='{$message_id}'>
-                    {$message[MESSAGE_KEY]} 
-                </div>
+                <button onclick='deleteMessage({$message_id})' class='del-button'>Delete</button>
+                <div class='message' onclick='copyMessage()' id='{$message_id}-m'>".nl2br($message[MESSAGE_KEY])."</div>
                 <br>
             </div>";
     }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" ) {
         if(isset($_POST['submitt-button']) && !isset($_COOKIE[COOKIE_NAME])){ //check if form was submitted and add the user's cookie
             $cookie_value = cleanString($_POST['device']); //get input text
             setcookie(COOKIE_NAME, $cookie_value, time() + (86400 * 365), "/");
@@ -82,7 +80,7 @@
             unset($_COOKIE[COOKIE_NAME]);
             setcookie(COOKIE_NAME, "", time() - 3600, '/');
             header("refresh:0");
-        } elseif (isset($_POST['message']) && $_POST[MESSAGE_KEY] != "") { // Post message to server if not empty
+        } elseif (isset($_POST['message']) && $_POST[MESSAGE_KEY] != "" && isset($_COOKIE[COOKIE_NAME])) { // Post message to server if not empty
             // Get the messages
             $messages = file_get_contents(MESSAGE_FILE);
             $messages_array = json_decode($messages, true);
@@ -94,12 +92,11 @@
             }
 
             // Generate the tags array and strip leading tags
-            $delimiter = " ";
             $tag_identifier = "#";
             $filter_identifier = "!";
 
             $message = trim($_POST[MESSAGE_KEY]);
-            $tokens = explode($delimiter, $message);
+            $tokens = preg_split("/[\s]+/", $message);  // Split by whitespace
             $tags = [];
             $removal_length = 0;
             $still_at_front = true;     // Indicates that the tags are still at the front of the string and the length should be counted for removal
@@ -107,7 +104,6 @@
 
                 if ($token != "") {     // Ignore double (or triple or quadruple or pentup--...) spaces
                     if ($token[0] == $tag_identifier) {
-                        echo $token;
                         $tags[] = substr(cleanString($token), 1);
                         if ($still_at_front) {
                             $removal_length += strlen($token) + 1;    // Only remove the hashes from the front of the string, ones in the middle should be left alone
@@ -123,10 +119,11 @@
                     }
                 }
             }
-
             if ($removal_length != 0) {
                 $message = substr($message, $removal_length - 1);   // -1 just in case there is no space after the last tag            
             }
+            // $message = ltrim($message);
+            
 
             if ($message != "" && $message != $filter_identifier && $message_filter == "") {   // Would not want a bunch of empty messages! (and do not add searches)
                 // JSON data entry
@@ -142,8 +139,11 @@
 
                 // Return to json and put updated to our file
                 $messages_array = json_encode($messages_array, JSON_PRETTY_PRINT);
-                file_put_contents(MESSAGE_FILE, $messages_array);
+                file_put_contents(MESSAGE_FILE, $messages_array, LOCK_EX);
             }
+
+            exit(0);  // Exit, do not display webpage
+
         } elseif (isset($_POST['delete-message'])) {
             // Get message
             $messages = file_get_contents(MESSAGE_FILE);
@@ -154,7 +154,9 @@
 
             // Return to json and put updated to our file
             $messages_array = json_encode($messages_array, JSON_PRETTY_PRINT);
-            file_put_contents(MESSAGE_FILE, $messages_array);
+            file_put_contents(MESSAGE_FILE, $messages_array, LOCK_EX);
+
+            exit(0);  // Exit, do not display webpage
         }
         
         $header = "location: messenger.php";
@@ -184,6 +186,7 @@
                 if ($message[TIME_KEY] > $time) {
                     $return_messages[] = array(
                         "html" => generateMessageHTML($message_id, $message),
+                        DEVICE_KEY => $message[DEVICE_KEY],
                         "time" => $message[TIME_KEY]
                     );
                 } 
@@ -199,311 +202,10 @@
 <head>
     <title>Zach's Messenger</title>
     <link rel="shortcut icon" href="favicon.ico"/>
+    <link rel="stylesheet" href="messenger-style.css">
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta content="utf-8" http-equiv="encoding">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-
-        html {
-            margin: 0;
-            padding: 0;
-        }
-
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 17px;
-            background-color: #333;
-            margin: 0;
-            padding: 0;
-        }
-
-        * {
-            box-sizing: border-box;
-        }
-
-        /* Full-width input fields */
-        input[type=text]:not(#message-box) {
-            width: 100%;
-            padding: 15px;
-            margin: 5px 0 22px 0;
-            display: inline-block;
-            border: none;
-            background: #f1f1f1;
-        }
-
-        /* Add a background color when the inputs get focus */
-        input[type=text]:focus {
-            background-color: #fff;
-            outline: none;
-        }
-
-        /* Set a style for all buttons */
-        button:not(.hidden) {
-            background-color: #3399ff;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-            opacity: 0.9;
-            font-size: 17px;
-        }
-
-        .signupbtn {
-            float: right;
-            width: 50%;
-        }
-
-        /* Add padding to container elements */
-        .container {
-            padding: 16px;
-        }
-
-        /* Modal Content/Box */
-        .modal-content {
-            background-color: #e0e0e0;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            border: 2px solid black;
-            width: 50%; /* Could be more or less, depending on screen size */
-            min-width: 300px;
-        }
-
-
-        /* Clear floats */
-        .clearfix::after {
-            content: "";
-            clear: both;
-            display: table;
-        }
-
-        /* Change styles for cancel button and signup button on extra small screens */
-        @media screen and (max-width: 300px) {
-            .signupbtn {
-                width: 100%;
-            }
-        }
-
-        /* NAV BAR / HEADER THING */
-        .navbar {
-            overflow: hidden;
-            background-color: #333;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: 1;
-        }
-
-        .navbar form, .navbar strong, .navbar i {
-            display: block;
-            color: #f2f2f2;
-            text-align: center;
-            text-decoration: none;
-        }
-
-        .navbar form {
-            float: right;
-            margin-right: 20px;
-        }
-
-        .navbar strong {
-            font-size: 20px;
-            font-weight: bold;
-            float: center;
-            padding: 20px;
-            margin: 0 0 -40px 0;
-            height: 40px;
-        }
-
-        .navbar i {
-            float: left;
-            padding: 20px;
-        }
-
-        /* BOTTOM MESSAGE BAR */
-        .message-container {
-            overflow: hidden;
-            background-color: #333;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-        }
-
-        #message-box {
-            padding: 15px;
-            margin: 12px 0 10px 10px;
-            display: inline;
-            border: none;
-            background: #f1f1f1;   
-            overflow: hidden;
-            width: calc(100% - 115px); 
-            font-size: inherit;
-        }
-
-        .notification {
-            position: absolute;
-            right: 5vw;
-            bottom: 100px;
-            background-color: #333;
-            font-family: Operator Mono A,Operator Mono B,Source Code Pro,Menlo,Consolas,Monaco,monospace;
-            text-align: center;
-            padding: 20px;
-            color: white;
-            border: 3px solid red;
-            border-radius: 10px;
-            cursor: pointer;
-            display: none;
-        }
-
-        button.hidden {
-            background-color: #3399ff;
-            color: white;
-            padding: 15px 20px;
-            margin: 12px 10px 10px 0;
-            border: none;
-            cursor: pointer;
-            opacity: 0.9;
-            float: right;
-            display: inline-block;
-            font-size: inherit;
-        }
-
-        button:hover { /* Also needed for logout and signup */
-            background-color: darkgreen;
-        }
-
-        button.hidden:disabled {
-            background-color: grey;
-            cursor: not-allowed;
-        }
-
-        /* CHAT CONTAINER - where the messages are displayed */
-        .chat-container {
-            margin: 53px 10px 53px 10px;
-            height: calc(100vh - (53px * 2));
-            overflow-y: scroll;
-            overflow-x: hidden;
-            background-color: black;
-            word-wrap: break-word; /* For Mitchel */
-            /* padding: 0 0 50px 0; */
-            /* position: fixed; */
-        }
-
-        /* Small screens */
-        @media only screen and (max-height: 600px) {
-            .chat-container {
-                height: 70vh;
-            }
-        }
-
-        .bottom {
-            height: 0px;
-            margin: 20px;
-        }
-
-        /* Holds the messages */
-        .chat-container .name {
-            color: #30ce2d;
-            display: inline-block;
-            margin-left: 10px;
-        }
-
-        .chat-container .time {
-            color: #c0c0e0;
-            display: inline-block;
-            margin-left: 10px;
-            font-size: 13px;
-        }
-
-        .chat-container .message {
-            color: white;
-            margin: 0 10px 0 10px;
-            cursor: copy;
-        }
-
-        .chat-container hr {
-            margin-left: 5px;
-            margin-right: 5px;
-        }
-
-        /* Tag styling */
-        .tags {
-            display: inline-block;
-            border-radius: 100px;
-            border: 3px solid;
-            color: white;
-            /* text-shadow: 0 0 5px black; */
-            font-family: "Times New Roman", Times, serif;
-            padding: 3px 7px;
-            margin: 0 0 3px 10px;
-        }
-
-        .tags-container {
-            display: inline-block;
-            margin: 0 0 10px 0;
-            padding: 0;
-        }
-
-        /* Delete button */
-        .inline-form {
-            display: inline-block;
-        }
-
-        input[type="submit"] {
-            background-color: red;
-            border: 1px solid grey;
-            color: white;
-            margin-left: 10px;
-            cursor: pointer;
-            visibility: hidden;
-        }
-
-        input[type="submit"]:hover {
-            background-color: darkred;
-        }
-
-        .single-message:hover .inline-form input[type="submit"] { 
-            visibility: visible;
-        }
-
-        /* Message for "no messages" */
-        #empty-message-container {
-            font-family: Operator Mono A,Operator Mono B,Source Code Pro,Menlo,Consolas,Monaco,monospace;
-            font-style: italic;
-            text-align: center;
-            padding: 30px;
-            color: white;
-        }
-        
-        #empty-message-container a {
-            text-decoration: none;
-            color: inherit;
-        }
-
-        @media only screen and (max-width: 550px) {
-            .navbar strong {
-                display: none;
-            }
-
-            #logout-button {
-                padding: 10px;
-            }
-
-            .notification {
-                padding: 10px;
-            }
-
-            /* See below for .tags and .tags-container */
-            .tags-container {
-                display: block;
-                margin: -10px 0 5px 0;
-            }
-        }
-    </style>
 </head>
 <body>
 <?php
@@ -623,16 +325,15 @@
 
         <!-- Display the message bar at the bottom of the page -->
         <div class="message-container">
-            <form action="" method="post">
-                <input id="message-box" type="text" placeholder="Say something..." name="message" autocomplete="off" oninput="enableSubmit()" autofocus/>
-                <button id="send-button" class="hidden" type="submit" disabled>Send!</button>
-            </form>
+            <textarea id="message-box" placeholder="Say something..." name="message" autocomplete="off" autofocus></textarea>
+            <button id="send-button" type="submit" onclick="postMessage()" disabled>Send!</button>
         </div>
 
         <script>
             // Autofocus the messaging box ("autofocus" does not work)
             window.onload = function() {
                 document.getElementById("message-box").focus();
+                enableSubmit();
             }
 
             // Every 2 seconds, update the messages display
@@ -684,7 +385,7 @@
 
             // Scroll to bottom of page smoothly
             function scrollToBottom() {
-                document.getElementById("bottom").scrollIntoView({behavior: "smooth", block: "end"});  // Go to bottom
+                document.getElementById("bottom").scrollIntoView({behavior: "auto", block: "end", inline: "nearest"});  // Go to bottom
                 hideNotification();
             }
             
@@ -714,6 +415,66 @@
                 } else { 
                     button.disabled = true; 
                 } 
+            }
+
+            function postMessage() {
+                let postButton = document.getElementById("send-button");
+                let messageBox = document.getElementById("message-box");
+                if (postButton.disabled === false && messageBox.value.trim() != "") {
+                    const messageData = new FormData();
+                    messageData.append('message', messageBox.value);
+
+                    fetch('messenger.php', {
+                        method: 'POST',
+                        body: messageData
+                    })
+                    .then(result => { 
+                        console.log('Success: ', result); 
+                        messageBox.value = "";  // Clear message
+                        postButton.disabled = true;
+                    })
+                    .catch(error => {
+                        console.log("Error: ", error);
+                        alert("There was a problem posting your message:\n" + error);
+                    });
+
+                }
+            }
+                // <form action='' method='post' class='inline-form'>
+                //     <input type='text' value='{$message_id}' name='message-id' style='display: none;'>
+                //     <input type='submit' name='delete-message' value='Delete'>
+                // </form>
+            function deleteMessage(messageId) {
+                if (confirm("Are you sure you want to delete this message?")) {
+                    const deleteData = new FormData();
+                    deleteData.append('delete-message', "");
+                    deleteData.append('message-id', messageId);
+                    fetch('messenger.php', {
+                        method: 'POST',
+                        body: deleteData
+                    })
+                    .then(result => {
+                        console.log("Message: '" + messageId + "' has been deleted");
+                        document.getElementById(messageId).remove();    // Remove from user's view
+                    })
+                    .catch(error => {
+                        console.log("Error: ", error);
+                        alert("There was a problem deleting message '" + messageId + "'\n");
+                    });
+                }
+            }
+
+            document.addEventListener('keyup', keyHandler);
+
+            function keyHandler(e) {
+                
+                enableSubmit();
+
+                // document.getElementById("message-box").focus();
+
+                if (e.ctrlKey && e.keyCode == 13) {  // Ctrl + Enter
+                    postMessage();
+                }
             }
 
             </script>
